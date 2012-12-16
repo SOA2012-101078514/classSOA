8e16a06e4f8ad7e641e41886fdbfc1ba1e17bcbf
yh lee <yoyo333lee@gmail.com>
2012-12-17 00:01:17 +0800

    1216

--- myrottenpotatoes/app/controllers/application_controller.rb
+++ myrottenpotatoes/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@
 
 class ApplicationController < ActionController::Base
 # before_filter :set_current_user
-  protected # prevents method from being invoked by a route
+  #protected # prevents method from being invoked by a route
   #def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
     #@current_user ||= Moviegoer.find_by_id(session[:user_id])
